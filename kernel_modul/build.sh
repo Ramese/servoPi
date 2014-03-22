9a88b3499859c5fb2b31f52bf25fa3d682decdd5
Radek Mečiar <Ramese@users.noreply.github.com>
2014-03-22 04:12:15 +0100

    build.sh vymazani promennych

--- kernel_modul/build.sh
+++ kernel_modul/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-ARCH=arm
-CROSS_COMPILE=/media/Data/KERNEL/tools-master/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-
-INSTALL_MOD_PATH=/media/Data/KERNEL/ModulZkouska/
+
 make -C /media/Data/KERNEL/kompilace/linux-rpi-3.10.y M=$(pwd) modules
