0b74a011ada861970b36f6ccf5702c848202f6a7
Radek Mečiar <r.meciar@gmail.com>
2014-03-26 21:34:58 +0100

    Uprava build

--- kernel_modul/build.sh
+++ kernel_modul/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+export ARCH=arm
+export CROSS_COMPILE=/media/Data/KERNEL/tools-master/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-
+export INSTALL_MOD_PATH=/media/Data/KERNEL/ModulZkouska/
+
 make -C /media/Data/KERNEL/kompilace/linux-rpi-3.10.y M=$(pwd) modules
